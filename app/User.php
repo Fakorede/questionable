4b6f1f89d3ac672aab9ae0864c37bf6a7d77c0cf
fakorede <abiolafakorede@gmail.com>
2019-11-08 16:00:02 +0100

    created the answer schema

--- app/User.php
+++ app/User.php
@@ -27,9 +27,16 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
-    public function questions() {
+    public function questions() 
+    {
         return $this->hasMany(Question::class);
     }
+
+    public function answers()
+    {
+        return $this->hasMany(Answer::class);
+    }
+
 
     public function getUrlAttribute()
     {
