dde32fa1a5477bd075fc67a51f347ee4f4890311
fakorede <abiolafakorede@gmail.com>
2019-11-09 23:55:06 +0100

    implementing best answer

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -3,59 +3,71 @@
         <div class="card">
             <div class="card-body">
                 <div class="card-title">
-                    <h2>{{ $answersCount . " " . str_plural('Answer',$question->answers_count ) }}</h2>
+                    <h2>{{ $answersCount . " " . str_plural('Answer', $answersCount) }}</h2>
                 </div>
                 <hr>
-
-                @include('layouts._messages')
+                @include ('layouts._messages')
                 
                 @foreach ($answers as $answer)
                     <div class="media">
-                        <div class="d-flex flex-column vote-controls">
+                        <div class="d-fex flex-column vote-controls">
                             <a title="This answer is useful" class="vote-up">
                                 <i class="fas fa-caret-up fa-3x"></i>
                             </a>
-                            <span class="votes-count">1200</span>
+                            <span class="votes-count">1230</span>
                             <a title="This answer is not useful" class="vote-down off">
                                 <i class="fas fa-caret-down fa-3x"></i>
                             </a>
-                            <a title="Mark as best answer" class="vote-accepted mt-2">
-                                <i class="fas fa-check fa-2x"></i>
-                                <span class="favorites-count">5</span>
-                            </a>
+                            @can ('accept', $answer)
+                                <a title="Mark this answer as best answer" 
+                                    class="{{ $answer->status }} mt-2"
+                                    onclick="event.preventDefault(); document.getElementById('accept-answer-{{ $answer->id }}').submit();"
+                                    >
+                                    <i class="fas fa-check fa-2x"></i>                                    
+                                </a>
+                                <form action="{{ route('answers.accept', $answer->id) }}" id="accept-answer-{{ $answer->id }}" method="POST" style="display:none;">
+                                    @csrf
+                                </form>
+                            @else
+                                @if ($answer->is_best)
+                                    <a title="The question owner accepted this answer as best answer" 
+                                        class="{{ $answer->status }} mt-2"                                        
+                                        >
+                                        <i class="fas fa-check fa-2x"></i>                                    
+                                    </a>
+                                @endif
+                            @endcan
                         </div>
                         <div class="media-body">
                             {!! $answer->body_html !!}
                             <div class="row">
                                 <div class="col-4">
                                     <div class="ml-auto">
-                                        @can("update", $answer)
+                                        @can ('update', $answer)
                                             <a href="{{ route('questions.answers.edit', [$question->id, $answer->id]) }}" class="btn btn-sm btn-outline-info">Edit</a>
                                         @endcan
-                                        @can("delete", $answer)
-                                            <form class="form-delete" method="post" action="{{ route('questions.answers.destroy',  [$question->id, $answer->id]) }}">
+                                        @can ('delete', $answer)
+                                            <form class="form-delete" method="post" action="{{ route('questions.answers.destroy', [$question->id, $answer->id]) }}">
                                                 @method('DELETE')
                                                 @csrf
-                                                <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure you want to delete?')">Delete</button>
+                                                <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">Delete</button>
                                             </form>
                                         @endcan
                                     </div>
                                 </div>
                                 <div class="col-4"></div>
                                 <div class="col-4">
-                                    <span class="text-muted">Answer {{ $answer->created_date }} By</span>
+                                    <span class="text-muted">Answered {{ $answer->created_date }}</span>
                                     <div class="media mt-2">
                                         <a href="{{ $answer->user->url }}" class="pr-2">
-                                            <img src="{{ $answer->user->avatar }}" alt="{{ $answer->user->name }}">
+                                            <img src="{{ $answer->user->avatar }}">
                                         </a>
                                         <div class="media-body mt-1">
-                                            <a href="{{ $answer->user->url }}">
-                                                {{ $answer->user->name }}
-                                            </a>
+                                            <a href="{{ $answer->user->url }}">{{ $answer->user->name }}</a>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
+                            </div>                            
                         </div>
                     </div>
                     <hr>
