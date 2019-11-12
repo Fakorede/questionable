2557a9b1e7ae404883298217a493e5689bc5748c
fakorede <abiolafakorede@gmail.com>
2019-11-12 03:07:18 +0100

    implemented vote answer controls

--- database/seeds/VotablesTableSeeder.php
+++ database/seeds/VotablesTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Question;
+use App\Answer;
 use App\User;
 use Illuminate\Database\Seeder;
 
@@ -13,7 +14,7 @@ class VotablesTableSeeder extends Seeder
      */
     public function run()
     {
-        \DB::table('votables')->where('votable_type', 'App\Question')->delete();
+        \DB::table('votables')->delete();
 
         $users = User::all();
         $usersCount = $users->count();
@@ -25,6 +26,15 @@ class VotablesTableSeeder extends Seeder
             {
                 $user = $users[$i];
                 $user->voteQuestion($question, $votes[rand(0, 1)]);
+            }
+        }
+
+        foreach (Answer::all() as $answer)
+        {
+            for ($i = 0; $i < rand(1, $usersCount); $i++)
+            {
+                $user = $users[$i];
+                $user->voteAnswer($answer, $votes[rand(0, 1)]);
             }
         }
     }
